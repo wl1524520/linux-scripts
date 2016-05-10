0ce2986e66d18e225d49fec0bfff404ea455589b
Wang Lu <wl1524520@gmail.com>
2016-05-11 02:48:12 +0800

    update zsh shell

--- zsh/personalise_zsh.sh
+++ zsh/personalise_zsh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cp -f conf/zshrc ~/.zshrc
-cp -f conf/wanglu.zsh-theme ~/.oh-my-zsh/themes
+#cp -f conf/zshrc ~/.zshrc
+#cp -f conf/wanglu.zsh-theme ~/.oh-my-zsh/themes
+echo "setopt no_nomatch" >> ~/.zshrc
