b6e739e208ddbd8941c2f3665a88aee115907afb
Wang Lu <wanglu@ynuvs.com>
2016-06-02 17:34:25 +0800

    update gentoo

--- linux-init/gentoo/04_kernel.sh
+++ linux-init/gentoo/04_kernel.sh
@@ -17,3 +17,20 @@ cat etc/fstab > /etc/fstab
 echo "gentoo" > /etc/hostname
 
 emerge --ask --noreplace net-misc/netifrc
+
+# install some service
+emerge --ask app-admin/sysklogd
+rc-update add sysklogd default
+
+emerge --ask sys-process/cronie
+rc-update add cronie default
+crontab /etc/crontab
+
+emerge --ask sys-apps/mlocate
+rc-update add sshd default
+
+emerge --ask net-misc/dhcpcd
+
+emerge --ask sys-boot/grub:2
+grub2-install /dev/sda
+grub2-mkconfig -o /boot/grub/grub.cfg
