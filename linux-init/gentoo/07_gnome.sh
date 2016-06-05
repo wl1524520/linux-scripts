4f91e85ec4d532a0a1eb28f0c79611be8e27afda
Wang Lu <wanglu@ynuvs.com>
2016-06-05 10:54:01 +0800

    update gnome

--- linux-init/gentoo/07_gnome.sh
+++ linux-init/gentoo/07_gnome.sh
@@ -3,11 +3,16 @@
 # Install gnome3
 emerge --ask gnome-base/gnome
 
+# update env
+env-update && source /etc/profile
+
 # Install cinnamon
 emerge --ask gnome-extra/cinnamon
 
+# Troubleshooting
+emerge --ask gnome-extra/gnome-tweak-tool
+emerge --ask gnome-extra/gnome-shell-extensions
+
 # Install Google Noto Fonts
 emerge --ask media-fonts/noto
-
-echo 'RUN: env-update && source /etc/profile'
 
