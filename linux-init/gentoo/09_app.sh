ca9dae5c4d8af5417060f4c29d2485d613f7d858
Wang Lu <wl1524520@gmail.com>
2016-06-07 17:17:38 +0800

    update app

--- linux-init/gentoo/09_app.sh
+++ linux-init/gentoo/09_app.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 emerge --ask gnome-extra/gnome-calculator media-gfx/gnome-screenshot media-gfx/eog gnome-extra/gnome-system-monitor app-arch/file-roller app-editors/gedit
+emerge --ask media-video/vlc media-sound/rhythmbox
