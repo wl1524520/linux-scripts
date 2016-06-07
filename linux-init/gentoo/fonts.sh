0d974b767c18fbbc0cac27142af6bc5bc493327a
Wang Lu <wl1524520@gmail.com>
2016-06-07 17:34:58 +0800

    add fonts

---
+++ linux-init/gentoo/fonts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+emerge --ask media-fonts/arphicfonts media-fonts/bitstream-cyberbit media-fonts/droid media-fonts/ipamonafont media-fonts/ja-ipafonts media-fonts/takao-fonts media-fonts/wqy-microhei media-fonts/wqy-zenhei
+
