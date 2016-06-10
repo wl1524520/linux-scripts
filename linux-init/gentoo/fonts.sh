ee7144e9c6c15d810ff389f80c77d0b60bcae883
Wang Lu <wl1524520@gmail.com>
2016-06-10 10:09:36 +0800

    update fonts install scripts

--- linux-init/gentoo/fonts.sh
+++ linux-init/gentoo/fonts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-emerge --ask media-fonts/arphicfonts media-fonts/bitstream-cyberbit media-fonts/droid media-fonts/ipamonafont media-fonts/ja-ipafonts media-fonts/takao-fonts media-fonts/wqy-microhei media-fonts/wqy-zenhei
+emerge --ask \
+    media-fonts/arphicfonts \
+    media-fonts/bitstream-cyberbit \
+    media-fonts/droid \
+    media-fonts/ipamonafont \
+    media-fonts/ja-ipafonts \
+    media-fonts/takao-fonts \
+    media-fonts/wqy-microhei \
+    media-fonts/wqy-zenhei
 
