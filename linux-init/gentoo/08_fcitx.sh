88ba6519c7419acb7c513c65c79f9659e2a99ece
Wang Lu <wl1524520@gmail.com>
2016-06-12 11:27:56 +0800

    update fcitx scripts

--- linux-init/gentoo/08_fcitx.sh
+++ linux-init/gentoo/08_fcitx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'app-i18n/fcitx X table nls gtk3' > /etc/portage/package.use/fcitx
 emerge --ask app-i18n/fcitx app-i18n/fcitx-configtool
 
 cp fcitx.xprofile /home/wilson/.xprofile && chown wilson:wilson /home/wilson/.xprofile
