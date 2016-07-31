dd986b03e97f3230d2e64f9bb6518083e7a2802e
Wang Lu <wl1524520@gmail.com>
2016-07-31 13:50:57 +0800

    update centos7

--- linux-init/centos7.sh
+++ linux-init/centos7.sh
@@ -3,6 +3,11 @@ yum update -y
 yum install -y wget net-tools git vim
 yum remove -y system-config-kdump
 
-echo -e "\nalias vi='vim'" >> /etc/profile
+#echo -e "\nalias vi='vim'" >> /etc/profile
 
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config 
+#sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config 
+
+# replace firewalld with iptables
+systemctl stop firewalld
+systemctl disable firewalld
+yum install -y iptables-services && systemctl enable iptables && systemctl start iptables
