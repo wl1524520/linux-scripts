2c25dd498e416c74d57a50b04d5510e4517ec827
Wang Lu <wl1524520@gmail.com>
2022-09-18 00:31:26 +0800

    update

--- ubuntu-init-php74.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update \
-    && sudo apt-get install -y nginx php7.4-fpm php7.4-cli php7.4-dev \
-       php7.4-pgsql php7.4-sqlite3 php7.4-gd \
-       php7.4-curl php7.4-memcached \
-       php7.4-imap php7.4-mysql php7.4-mbstring \
-       php7.4-xml php7.4-zip php7.4-bcmath php7.4-soap \
-       php7.4-intl php7.4-readline php7.4-pcov \
-       php7.4-msgpack php7.4-igbinary php7.4-ldap \
-       php7.4-redis
