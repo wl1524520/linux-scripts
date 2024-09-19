e6e7428e88897c55a5d70957f03a23696a10807b
Wang Lu <wl1524520@gmail.com>
2024-09-20 00:58:43 +0800

    update

--- php83-ubuntu.sh
+++ php83-ubuntu.sh
@@ -2,7 +2,7 @@
 
 # https://launchpad.net/~ondrej/+archive/ubuntu/php
 
-sudo apt-get install php8.3-cli php8.3-dev \
+sudo apt-get install php8.3-fpm php8.3-cli php8.3-dev \
        php8.3-pgsql php8.3-sqlite3 php8.3-gd \
        php8.3-curl php8.3-memcached \
        php8.3-imap php8.3-mysql php8.3-mbstring \
